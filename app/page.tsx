16821d3825f01e7d2451275781e227bec654bf17
VictorJGB <victorgb.dev@gmail.com>
2025-02-25 11:02:25 -0300

    initial changes

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <h1 className="text-4xl font-bold text-primary">
+        Fintrack
+      </h1>
+      <p>Gerenciamento financeiro
+      </p>
+    </div>
+  );
+}
