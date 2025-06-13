a416c38ecdcfc512c1548edf0854ac0ce3a2dd00
VictorJGB <victorgb.dev@gmail.com>
2025-06-13 17:31:01 -0300

    feat: Integrating api with expenses chart

--- components/charts/incomes-chart.tsx
+++ components/charts/incomes-chart.tsx
@@ -2,8 +2,11 @@
 
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 
+import getExpenses from "@/actions/expenses/get-expenses"
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+import { useQuery } from "@tanstack/react-query"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Skeleton } from "../ui/skeleton"
 
 const chartData = [
   { date: "2024-04-01", desktop: 222 },
@@ -101,75 +104,94 @@ const chartData = [
 
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "var(--primary)",
+  expenses: {
+    label: "Valor",
+    color: "var(--destructive)",
   },
 } satisfies ChartConfig
 
 export default function IncomesChart() {
-  return (
-    <Card className="rounded-2xl w-full">
-      <CardHeader>
-        <CardTitle>Total Visitors</CardTitle>
-        <CardDescription>Total for the last 3 mounths</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig} className="aspect-auto h-[300px] w-full">
-          <AreaChart data={chartData}>
-            <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-            </defs>
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              minTickGap={32}
-              tickFormatter={(value) => {
-                const date = new Date(value)
-                return date.toLocaleDateString("pt-BR", {
-                  month: "short",
-                  day: "numeric",
-                })
-              }}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={
-                <ChartTooltipContent
-                  labelFormatter={(value) => {
-                    return new Date(value).toLocaleDateString("pt-BR", {
-                      month: "short",
-                      day: "numeric",
-                    })
-                  }}
-                  indicator="dot"
-                />
-              }
-            />
-            <Area
-              dataKey="desktop"
-              type="natural"
-              fill="url(#fillDesktop)"
-              stroke="var(--color-desktop)"
-              stackId="a"
-            />
-          </AreaChart>
-        </ChartContainer>
-      </CardContent>
-    </Card >
-  )
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["expenses"],
+    queryFn: getExpenses
+  })
+
+  if (isLoading) return <Skeleton className="rounded-2xl w-full h-[300px]" />
+
+  if (error) {
+    return (
+      <Card>
+        <CardContent>
+          <p className="text-destructive font-semibold">{JSON.stringify(error, null, 2)}</p>
+        </CardContent>
+      </Card>
+    )
+  }
+
+  if (data) {
+    return (
+      <Card className="rounded-2xl w-full">
+        <CardHeader>
+          <CardTitle>Total de despesas</CardTitle>
+          <CardDescription>Despesas do mês</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig} className="aspect-auto h-[300px] w-full">
+            <AreaChart data={data}>
+              <defs>
+                <linearGradient id="fillExpenses" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-expenses)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-expenses)"
+                    stopOpacity={0.1}
+                  />
+                </linearGradient>
+              </defs>
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                minTickGap={32}
+                tickFormatter={(value) => {
+                  const date = new Date(value)
+                  return date.toLocaleDateString("pt-BR", {
+                    month: "short",
+                    day: "numeric",
+                  })
+                }}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={
+                  <ChartTooltipContent
+                    labelFormatter={(value) => {
+                      return new Date(value).toLocaleDateString("pt-BR", {
+                        month: "short",
+                        day: "numeric",
+                      })
+                    }}
+                    indicator="dot"
+                  />
+                }
+              />
+              <Area
+                dataKey="amount_per_installment"
+                type="natural"
+                fill="url(#fillExpenses)"
+                stroke="var(--color-expenses)"
+                stackId="a"
+              />
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
+      </Card >
+    )
+  }
 }
