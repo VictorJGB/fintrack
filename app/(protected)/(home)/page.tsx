dc606fab1e0813e7c94d5560848d0cea084791b0
VictorJGB <victorgb.dev@gmail.com>
2025-06-13 15:22:13 -0300

    feat: Adding the incomes chart

--- app/(protected)/(home)/page.tsx
+++ app/(protected)/(home)/page.tsx
@@ -1,11 +1,11 @@
-import MainChart from "@/components/charts/main-chart";
+import IncomesChart from "@/components/charts/incomes-chart";
 import SectionCards from "@/components/section-cards";
 
 export default function Home() {
   return (
     <div className="@container/main flex flex-col items-start justify-start size-full gap-6">
       <SectionCards />
-      <MainChart />
+      <IncomesChart />
     </div>
   );
 }
