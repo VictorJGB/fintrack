74643b5e0548b3aafed9d237b50a83854c0063bc
VictorJGB <victorgb.dev@gmail.com>
2025-07-21 17:28:00 -0300

    feat: Integrating the expenses file uploader with form

--- components/dialogs/expenses/import-expense.tsx
+++ components/dialogs/expenses/import-expense.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useMemo } from 'react'
+// components
 import FileUploader from '@/components/file-uploader'
 import { Button } from "@/components/ui/button"
 import {
@@ -12,38 +16,89 @@ import {
 } from "@/components/ui/dialog"
 // icons
 import { FileUp } from "lucide-react"
+// forms
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  files: z.array(z.instanceof(File))
+    .min(1, "Selecione pelo menos um arquivo")
+    .max(1, "Máximo de 1 arquivo permitido")
+    .refine((files) => files.every((file) => file.size <= 2 * 1024 * 1024), {
+      message: "Arquivo deve ser menor que 2MB",
+      path: ["files"],
+    }),
+})
+
+interface FormValues extends z.infer<typeof formSchema> { }
 
 export default function ImportExpenseDialog() {
+  const MAX_FILES = 1
+
+
+  // form
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      files: [],
+    },
+  })
+
+  const isFilesUploaded = useMemo(() => {
+    console.log(form.getValues("files"))
+    return form.watch("files")?.length > 0
+  }, [])
+
+  function onSubmit(data: FormValues) {
+    console.log("Form submitted with data:", data);
+  }
+
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button variant="outline">
-            <FileUp className="mr-2 h-4 w-4" />
-            Importar
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[500px]">
-          <DialogHeader>
-            <DialogTitle>Importar despesa</DialogTitle>
-            <DialogDescription>
-              Importe suas despesas a partir de um arquivo ou Excel.
-            </DialogDescription>
-          </DialogHeader>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          Importar
+          <FileUp className="ml-2 h-4 w-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[800px]" onInteractOutside={() => form.reset()}>
+        <DialogHeader>
+          <DialogTitle>Importar despesa</DialogTitle>
+          <DialogDescription>
+            Importe suas despesas a partir de um arquivo ou Excel.
+          </DialogDescription>
+        </DialogHeader>
 
-          <div className='flex items-center justify-center w-full mt-6'>
-            <FileUploader />
-          </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+            <FormField
+              control={form.control}
+              name="files"
+              render={({ field }) => (
+                <FormItem className='w-full'>
+                  <FormControl>
+                    <FileUploader
+                      formValue={field.value}
+                      onFormValueChange={field.onChange}
+                      maxFiles={MAX_FILES}
+                      disabled={isFilesUploaded}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
 
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-
-        </DialogContent>
-      </form>
-    </Dialog>
+            <DialogFooter>
+              <DialogClose asChild onClick={() => form.reset()}>
+                <Button variant="outline">Fechar</Button>
+              </DialogClose>
+              <Button type="submit" disabled={!isFilesUploaded}>Importar arquivo</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog >
   )
 }
--- components/file-uploader.tsx
+++ components/file-uploader.tsx
@@ -15,14 +15,28 @@ import { Upload, X } from "lucide-react";
 import * as React from "react";
 import { toast } from "sonner";
 
-export default function FileUploader() {
+interface Props {
+  formValue?: File[];
+  onFormValueChange?: (files: File[]) => void;
+  maxFiles: number
+  disabled?: boolean;
+}
+
+export default function FileUploader({ formValue, onFormValueChange, maxFiles, disabled }: Props) {
   const [files, setFiles] = React.useState<File[]>([]);
+
+  const isMaxFilesReached = React.useMemo(() => {
+    return files.length >= maxFiles;
+  }, [files]);
 
   const onFileValidate = React.useCallback(
     (file: File): string | null => {
 
       // Validate file type (only images)
-      if (!file.type.startsWith(".xlsx") || !file.type.startsWith(" .xls")) {
+      if (
+        file.type !== "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" && // .xlsx
+        file.type !== "application/vnd.ms-excel" // .xls
+      ) {
         return "Apenas arquivos Excel (.xlsx, .xls) são permitidos";
       }
 
@@ -38,41 +52,53 @@ export default function FileUploader() {
   );
 
   const onFileReject = React.useCallback((file: File, message: string) => {
-    toast(message, {
-      description: `"${file.name.length > 20 ? `${file.name.slice(0, 20)}...` : file.name}" has been rejected`,
+    toast.error(message, {
+      description: `Arquivo "${file.name.length > 20 ? `${file.name.slice(0, 20)}...` : file.name}" foi rejeitado`,
     });
   }, []);
 
   return (
     <FileUpload
-      value={files}
-      onValueChange={setFiles}
+      value={formValue ?? files}
+      onValueChange={onFormValueChange ?? setFiles}
       onFileValidate={onFileValidate}
       onFileReject={onFileReject}
-      accept="image/*"
-      maxFiles={2}
-      className="w-full max-w-md"
-      multiple
+      disabled={disabled}
+      accept=".xlsx, .xls"
+      maxFiles={maxFiles}
+      className="w-full"
     >
       <FileUploadDropzone>
         <div className="flex flex-col items-center gap-1">
           <div className="flex items-center justify-center rounded-full border p-2.5">
             <Upload className="size-6 text-muted-foreground" />
           </div>
-          <p className="font-medium text-sm">Drag & drop files here</p>
+          <p className="font-medium text-sm">Arraste e solte o arquivo aqui</p>
           <p className="text-muted-foreground text-xs">
-            Or click to browse (max 2 files)
+            ou clique para buscar
           </p>
         </div>
         <FileUploadTrigger asChild>
           <Button variant="outline" size="sm" className="mt-2 w-fit">
-            Browse files
+            Buscar arquivo
           </Button>
         </FileUploadTrigger>
       </FileUploadDropzone>
       <FileUploadList>
-        {files.map((file) => (
-          <FileUploadItem key={file.name} value={file}>
+        {formValue && formValue.map((file, index) => (
+          <FileUploadItem key={`${file.name}-${index}`} value={file}>
+            <FileUploadItemPreview />
+            <FileUploadItemMetadata />
+            <FileUploadItemDelete asChild>
+              <Button variant="ghost" size="icon" className="size-7">
+                <X />
+              </Button>
+            </FileUploadItemDelete>
+          </FileUploadItem>
+        ))}
+
+        {!formValue && files.map((file, index) => (
+          <FileUploadItem key={`${file.name}-${index}`} value={file}>
             <FileUploadItemPreview />
             <FileUploadItemMetadata />
             <FileUploadItemDelete asChild>
