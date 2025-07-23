cf0b64c86fec4e2b97b7dd399628740e35e5c639
VictorJGB <victorgb.dev@gmail.com>
2025-07-23 17:11:44 -0300

    fix: eslint

--- app/(protected)/incomes/page.tsx
+++ app/(protected)/incomes/page.tsx
@@ -1,5 +1,7 @@
 // components
 import IncomesTable from "@/components/tables/incomes";
+import TableSkeleton from "@/components/tables/table-skeleton";
+import { Suspense } from "react";
 
 export default function IncomesPage() {
 
@@ -7,7 +9,9 @@ export default function IncomesPage() {
     <div className="px-4 sm:px-10 size-full flex flex-col items-start justify-start">
       <h1 className="container text-2xl font-bold text-center md:text-start">Gerenciamento de recebimentos</h1>
 
-      <IncomesTable />
+      <Suspense fallback={<TableSkeleton rowsNumber={10} />}>
+        <IncomesTable />
+      </Suspense>
     </div>
   )
 }
