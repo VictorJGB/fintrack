4828975b651335378e599c7e169a52ee7e359409
VictorJGB <victorgb.dev@gmail.com>
2025-07-23 17:02:44 -0300

    fix: eslint

--- app/(protected)/expenses/page.tsx
+++ app/(protected)/expenses/page.tsx
@@ -1,6 +1,8 @@
 // components
 
 import ExpensesTable from "@/components/tables/expenses";
+import TableSkeleton from "@/components/tables/table-skeleton";
+import { Suspense } from "react";
 
 export default function ExpensesPage() {
 
@@ -8,7 +10,9 @@ export default function ExpensesPage() {
     <div className="px-4 sm:px-10 py-6 size-full flex flex-col items-start justify-start">
       <h1 className="container text-2xl font-bold">Gerenciamento de despesas</h1>
 
-      <ExpensesTable />
+      <Suspense fallback={<TableSkeleton rowsNumber={10} />}>
+        <ExpensesTable />
+      </Suspense>
     </div>
   )
 }
