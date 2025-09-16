3b0437ec46f2a4a115d22dee02d8e166ed756508
VictorJGB <victorgb.dev@gmail.com>
2025-09-16 17:10:05 -0300

    feat: Adding the report form

--- app/(protected)/reports/page.tsx
+++ app/(protected)/reports/page.tsx
@@ -1,7 +1,13 @@
+import ReportsForm from "@/components/forms/reports-form";
+
 export default function ReportsPage() {
   return (
-    <div className="size-full flex flex-col items-start justify-start">
-      <h1>Relatórios</h1>
+    <div className="px-4 sm:px-10 size-full flex flex-col items-start justify-start gap-6">
+      <h1 className="text-2xl font-bold text-center md:text-start">Relatórios</h1>
+
+      <div className="container flex items-start justify-start w-full">
+        <ReportsForm />
+      </div>
     </div>
   )
 }
---
+++ components/forms/reports-form.tsx
@@ -0,0 +1,157 @@
+'use client'
+
+// components
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Card, CardContent } from "@/components/ui/card";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+// libs
+import { cn } from "@/lib/utils";
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { CalendarIcon } from "lucide-react";
+// form
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  report: z.enum(['expenses', 'incomes']),
+  initial_date: z.date({ required_error: "Data é obrigatório" }),
+  final_date: z.date({ required_error: "Data é obrigatório" }),
+})
+
+export default function ReportsForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      report: undefined,
+      initial_date: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+      final_date: new Date()
+    }
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <Card className="rounded-2xl">
+          <CardContent className="grid grid-cols-1 grid-rows-3 lg:grid-cols-4 lg:grid-rows-1 gap-4 lg:gap-6">
+            {/* reports */}
+            <FormField
+              control={form.control}
+              name="report"
+              render={({ field }) => (
+                <FormItem className="lg:col-span-2">
+                  <FormLabel>Tipo de relatório</FormLabel>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Selecione um relatório" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="expenses">Total de despesas</SelectItem>
+                        <SelectItem value="incomes">Total de recebimentos</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+
+            {/* initial date */}
+            <FormField
+              control={form.control}
+              name="initial_date"
+              render={({ field }) => (
+                <FormItem className="grid gap-2">
+                  <FormLabel>Data inicial</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP", { locale: ptBR })
+                          ) : (
+                            <span>Escolha uma data</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-full p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        locale={ptBR}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* final date */}
+            <FormField
+              control={form.control}
+              name="final_date"
+              render={({ field }) => (
+                <FormItem className="grid gap-2">
+                  <FormLabel>Data final</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP", { locale: ptBR })
+                          ) : (
+                            <span>Escolha uma data</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-full p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        locale={ptBR}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+
+        <div className="w-full flex items-center justify-end mt-5"><Button type="submit">Gerar relatório</Button></div>
+      </form>
+    </Form>
+  )
+}
