08f947160849996f58fb3d095d92ac1ba7d4fed6
VictorJGB <victorgb.dev@gmail.com>
2025-12-01 17:11:04 -0300

    refactor: Grouped chart loading

--- components/charts/grouped-expenses-chart.tsx
+++ components/charts/grouped-expenses-chart.tsx
@@ -54,7 +54,9 @@ export default function GroupedExpensesChart({ className }: Props) {
   )), [data])
 
   useEffect(() => {
-    if (error) toast.error(error?.message || 'Erro ao carregar despesas agrupadas!')
+    if (error) toast.error("Gráficos de despesas agrupadas", {
+      description: error?.message || 'Erro ao carregar despesas agrupadas!'
+    })
 
     // Adding chart config
     if (data) {
@@ -73,69 +75,66 @@ export default function GroupedExpensesChart({ className }: Props) {
     }
   }, [data, error])
 
-  if (isLoading) return <Skeleton className={cn("rounded-2xl h-[300px]", className)} />
-
-  if (!isLoading) {
-    return (
-      <Card className={cn("flex flex-col rounded-2xl", className)}>
-        <CardHeader className="items-center pb-0">
-          <CardTitle>Despesas por destinatário</CardTitle>
-          <CardDescription>Visualize o total de despesas de cada destinatário</CardDescription>
-        </CardHeader>
-        <CardContent className="flex-1 pb-0">
-          {!chartData || chartData?.length === 0 && <p className="size-full text-center text-medium text-muted-foreground">Nenhuma despesa encontrada</p>}
-          {chartData && chartData?.length > 0 &&
-            <ChartContainer
-              config={chartConfig}
-              className="mx-auto aspect-square max-h-[300px] w-full [&_.recharts-pie-label-text]:fill-foreground pb-0"
-            >
-              <PieChart>
-                <ChartTooltip content={
-                  <ChartTooltipContent
-                    hideLabel
-                    formatter={(value, name) => {
-                      const amount = formatToBRL(Number(value))
-                      const recipient = Capitalize(name.toString())
-                      return <span>{recipient}: <strong>{amount}</strong></span>
-                    }
-                    }
-                    labelFormatter={(value) => formatToBRL(Number(value))}
-                  />
-                } />
-                <Pie
-                  data={chartData}
-                  dataKey="value"
-                  nameKey="_id"
-                  label={({ payload, ...props }) => {
-                    return (
-                      <text
-                        cx={props.cx}
-                        cy={props.cy}
-                        x={props.x}
-                        y={props.y}
-                        textAnchor={props.textAnchor}
-                        dominantBaseline={props.dominantBaseline}
-                        className="fill-foreground"
-                      >
-                        {formatToBRL(payload.value)}
-                      </text>
-                    )
-                  }}
-                />
-                <ChartLegend
-                  content={
-                    <ChartLegendContent
-                      nameKey="_id"
-                    />
+  return (
+    <Card className={cn("flex flex-col rounded-2xl", className)}>
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Despesas por destinatário</CardTitle>
+        <CardDescription>Visualize o total de despesas de cada destinatário</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        {isLoading && !chartData && <Skeleton className="h-[300px] w-full rounded-2xl" />}
+        {!chartData || chartData?.length === 0 && <p className="size-full text-center text-medium text-muted-foreground">Nenhuma despesa encontrada</p>}
+        {chartData && chartData?.length > 0 &&
+          <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[300px] w-full [&_.recharts-pie-label-text]:fill-foreground pb-0"
+          >
+            <PieChart>
+              <ChartTooltip content={
+                <ChartTooltipContent
+                  hideLabel
+                  formatter={(value, name) => {
+                    const amount = formatToBRL(Number(value))
+                    const recipient = Capitalize(name.toString())
+                    return <span>{recipient}: <strong>{amount}</strong></span>
                   }
-                  className="-translate-y-2 flex-wrap gap-2 *:basis-1/4 *:justify-center w-full"
+                  }
+                  labelFormatter={(value) => formatToBRL(Number(value))}
                 />
-              </PieChart>
-            </ChartContainer>
-          }
-        </CardContent>
-      </Card>
-    )
-  }
+              } />
+              <Pie
+                data={chartData}
+                dataKey="value"
+                nameKey="_id"
+                label={({ payload, ...props }) => {
+                  return (
+                    <text
+                      cx={props.cx}
+                      cy={props.cy}
+                      x={props.x}
+                      y={props.y}
+                      textAnchor={props.textAnchor}
+                      dominantBaseline={props.dominantBaseline}
+                      className="fill-foreground"
+                    >
+                      {formatToBRL(payload.value)}
+                    </text>
+                  )
+                }}
+              />
+              <ChartLegend
+                content={
+                  <ChartLegendContent
+                    nameKey="_id"
+                  />
+                }
+                className="-translate-y-2 flex-wrap gap-2 *:basis-1/4 *:justify-center w-full"
+              />
+            </PieChart>
+          </ChartContainer>
+        }
+      </CardContent>
+    </Card>
+  )
 
 }
